ad58daf7d3ac8450690b886e65a9e03e53b0849b
kabirulhassan <kabirulhassan7@gmail.com>
2022-05-11 19:46:25 +0530

    slider korte parchina

--- script.js
+++ script.js
@@ -230,3 +230,26 @@ setInterval(function(){
   container.innerHTML= tmp;
   }, 5500 // length of the whole show in milliseconds
   );
+
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 22,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    margin: 15,
+    nav: true,
+    navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
